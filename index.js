cdbe2ccaca5fadc73b896cac122506216fa15767
Aqeel Aloraibi <aqeel.aloraibi@gmail.com>
2021-05-10 09:27:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solutions in this file
 
+<<<<<<< HEAD
 function writeCards(names,events) {
     let messageArray=[];
     for (let i = 0; i <names.length; i++) {
@@ -16,3 +17,19 @@ while (i>=0){
 }
 }
 countDown(10);
+=======
+const names =['Lisa', 'Kaitlin', 'Jan' ];
+function writeCards(names){
+    for(let i=0;i<names.length;i++){
+        console.log([`Thank you, ${names[i]}, for the wonderful surprise gift!`]);
+    }
+}
+writeCards(names);
+
+function countdown(numb){
+let i=numb
+while (i>=0){
+    console.log(i--);
+}
+}
+>>>>>>> 3f9e6ce7fda766792a06ef498aa291934372f250
